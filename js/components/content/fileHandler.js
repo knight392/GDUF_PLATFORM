15f882131f05f8a80ba8051fbda816f8c2396f72
knight392 <knight_yya@163.com>
2021-04-07 20:32:58 +0800

    edit common method

--- js/components/content/fileHandler.js
+++ js/components/content/fileHandler.js
@@ -1,4 +1,11 @@
-//发送图片
+import request from '../../util/request.js'
+//文件上传
+/**
+ * 
+ * @param {*} url 
+ * @param {*} formdata 
+ * @returns (promise) res里保存的是文件上传后返回的远程地址
+ */
 export default function sendFile(url ,formdata) { //imgObj是jq对象
   return new Promise((resolve, reject) => {
     request(url, {
