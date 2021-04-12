7eb405d6e29e71cfd03df61edb01f0b62a5e0a84
piaoliangjiejie2019 <piaoliangjiejie2019@outlook.com>
2021-04-12 23:14:15 +0800

    edit logon

--- js/common/user/index.js
+++ js/common/user/index.js
@@ -1,6 +1,6 @@
-import {getLocalUser, setLocalUser, removeLocalUser, loginRequest} from './tools.js'
-import {createWebSocket, closeWebSocket} from '../../components/content/inform/index.js'
-import {baseWsURL} from '../baseRequestInfo.js'
+import { getLocalUser, setLocalUser, removeLocalUser, loginRequest } from './tools.js'
+import { createWebSocket, closeWebSocket } from '../../components/content/inform/index.js'
+import { baseWsURL } from '../baseRequestInfo.js'
 // user为null,表示没有登录
 /**
  *  { userType, markNumber, email, face, college, sex, userName, area, graduatedUniversity, degree }
@@ -8,24 +8,24 @@ import {baseWsURL} from '../baseRequestInfo.js'
 let user = null;
 
 // 每次加载页面都看看有没有token,可自动登录
-(function(){
-  const user = getLocalUser();
-  if(user != null){
-    try{
-      // 有token就发送一个token的cookie
-      createWebSocket(baseWsURL)
-    }catch(e) {
-      console.log(e);
-      user = null;
+(function() {
+    const user = getLocalUser();
+    if (user != null) {
+        try {
+            // 有token就发送一个token的cookie
+            createWebSocket(baseWsURL)
+        } catch (e) {
+            console.log(e);
+            user = null;
+        }
     }
-  }
 })()
 
 // 退出登录
-function doLogOff(){
-  user = null
-  closeWebSocket()
-  removeLocalUser()
+function doLogOff() {
+    user = null
+    closeWebSocket()
+    removeLocalUser()
 }
 /**
  * 
@@ -34,29 +34,24 @@ function doLogOff(){
  */
 
 // 登录
-function doLogin(loginData){
-  loginRequest(loginData).then(res => {
-    user = res
-    createWebSocket(baseWsURL)
-    setLocalUser(user)
-  }, err => {
-    user = null
-  })
+function doLogin(loginData) {
+    return new Promise((resolve, reject) => {
+        loginRequest(loginData).then(res => {
+            user = res
+            createWebSocket(baseWsURL)
+            setLocalUser(user)
+            resolve(true)
+        }, err => {
+            user = null
+            resolve(false)
+        })
+    })
+
 }
 
 // 判断是否用户登录了
 function isLogin() {
-  return user == null ? false : true
- }
+    return user == null ? false : true
+}
 
-export {user, doLogOff, doLogin, isLogin}
-
-
-
-
-
-
-
-
-
-
+export { user, doLogOff, doLogin, isLogin }
