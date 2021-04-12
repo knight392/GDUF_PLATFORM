7c886bf080c244dd29b48098b52bf4d401616560
knight392 <knight_yya@163.com>
2021-04-12 10:26:13 +0800

    edit userLogin

--- js/common/user/index.js
+++ js/common/user/index.js
@@ -1,11 +1,11 @@
-import {getToken, loginRequest, removeToken} from './tools.js'
+import {getLocalUser, setLocalUser, removeLocalUser, loginRequest} from './tools.js'
 // user为null,表示没有登录
 let user = null;
 
 // 每次加载页面都看看有没有token,可自动登录
 (function(){
-  const token = getToken();
-  if(token != null){
+  const user = getLocalUser();
+  if(user != null){
     try{
       // 有token就发送一个token的cookie
       user =  doLogin()
@@ -19,7 +19,7 @@ let user = null;
 // 退出登录
 function doLogOff(){
   user = null
-  removeToken()
+  removeLocalUser()
 }
 /**
  * 
@@ -31,6 +31,7 @@ function doLogOff(){
 function doLogin(loginData = null){
   loginRequest(loginData).then(res => {
     user = res
+    setLocalUser(user)
   }, err => {
     user = null
   })
