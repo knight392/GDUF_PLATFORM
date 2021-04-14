e67d4fd4171149bfcb2acc93dafb355277e08720
knight392 <knight_yya@163.com>
2021-04-14 20:08:30 +0800

    edit ip

--- js/common/baseRequestInfo.js
+++ js/common/baseRequestInfo.js
@@ -1,6 +1,6 @@
 /**
  * 保存远程请求的基本信息
  */
-const baseHttpURL = 'http://192.168.0.174:8080'
-const baseWsURL = 'ws://192.168.0.174:8080/WebSocket'
+const baseHttpURL = 'http://192.168.0.175:8080'
+const baseWsURL = 'ws://192.168.0.175:8080/WebSocket'
 export { baseHttpURL, baseWsURL }
