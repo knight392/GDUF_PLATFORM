f8c2bfc87bbe8abd0448e462c60c61ec24252279
knight392 <knight_yya@163.com>
2021-04-20 10:45:17 +0800

    finish 1.3

--- js/views/index/info.js
+++ js/views/index/info.js
@@ -98,7 +98,7 @@ export function infoIndexPART2() {
         json["grade"] = res.dataList[i].teacher.level;
       }
       const trend = template("schoolDevelopmentTrendsY-template", json);
-      $('#dynamicsY_container').prepend(trend);
+      $('#dynamicsY_container').append(trend);
 
       if (res.dataList[i].contents != null) {
         if (res.dataList[i].contents.length == 1 && res.dataList[i].contents[0].contentType === "video") {
